78ad69f04fe5fedce804fb2fd24d318bf9805ee5
Zoubir <marouf.zoubeir@gmail.com>
2020-08-11 23:10:11 +0200

    Ajout d'une page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,22 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * page d'accueil
+     * @Route("/", name="homepage")
+     */
+
+    public function home()
+    {
+        //Afficher le template Twig home.html.twig
+        return $this->render('home.html.twig', [
+            'foo' => 'bar',
+            'booleen' => true,
+            'produits' => ['Jean', 'Pull', 'Bonnet']
+        ]);
+    }
+
     /**
      * @Route("/{id}", name="home")  //URI
      */
@@ -21,21 +37,7 @@ class HomeController extends AbstractController
             'get' => $request->query->get('test', 'défaut'), //si n y a rien il nous renvoie 'defaut'
             // 'get2' => $_GET['test'] ?? 'defaut',    // ==> ?? on remplace les else
             'session' => get_class($session), 
+            'session' => $session->has('cle_inexistante'),
         ]);
     }
-
-    // public function index()
-    // {
-    //     return $this->json([
-    //         'test' => $request->query->get('foo')
-    //     ]);
-    // }
-
-    // public function index(Request $request, Session $session)
-    // {
-    //     return $this->json([
-    //         'test' => $request->query->get('test'),
-    //         'session' => $session->has('cle_inexistante')
-    //     ]);
-    // }
 }
