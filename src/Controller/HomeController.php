d62658a8f4a0cfe8e109c3670f81b4343fd20beb
Zoubir <marouf.zoubeir@gmail.com>
2020-08-12 23:26:22 +0200

    INSTALLE Doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,6 +11,35 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * page liste d'artistes
+     * @Route("/list", name="list")
+     */
+    public function list(Request $request)
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+            'get' => $request->query->get('test', 'défaut'), //si n y a rien il nous renvoie 'defaut'
+            // 'get2' => $_GET['test'] ?? 'defaut',    // ==> ?? on remplace les else
+        ]);
+    }
+     /**
+     * page connexion
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexion(Request $request, Session $session)
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+            'get' => $request->query->get('test', 'défaut'), //si n y a rien il nous renvoie 'defaut'
+            // 'get2' => $_GET['test'] ?? 'defaut',    // ==> ?? on remplace les else
+            'session' => get_class($session), 
+            'session' => $session->has('cle_inexistante'),
+        ]);
+    }
 
     /**
      * page d'accueil
@@ -26,18 +57,55 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="home")  //URI
+     * @Route("/test", name="test")
      */
-    public function index($id, Request $request, Session $session)
+    public function test(EntityManagerInterface $em)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-            'id' => $id,
-            'get' => $request->query->get('test', 'défaut'), //si n y a rien il nous renvoie 'defaut'
-            // 'get2' => $_GET['test'] ?? 'defaut',    // ==> ?? on remplace les else
-            'session' => get_class($session), 
-            'session' => $session->has('cle_inexistante'),
-        ]);
+        //Création d'une entité Produit
+        $produit = new Produit();
+        dump($produit);
+
+        //Enchainement de méthodes (method-chaining): l'objet se retourne lui-meme à chaque setter
+
+        $produit
+            ->setNom('Jeans')
+            ->setDescription('un super jean très résistant !!')
+            ->setPrix('55.00')
+            ->setQuantite(30)
+        ;
+        dump($produit); 
+
+        //Insertion en base
+        $em->persist($produit); //On prépare l'insertion
+        $em->flush();           //On enregistre en base
+        dump($produit); 
+
+        //Modification
+
+        $produit->setDescription('');
+        $em->flush();         
+        dump($produit); 
+
+        //Suppression
+
+        $em->remove($produit);  // On prépare à la suppression
+        $em->flush();         
+        dd($produit);
     }
+
+    // /**
+    //  * @Route("/{id}", name="home")  //URI
+    //  */
+    // public function index($id, Request $request, Session $session)
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/HomeController.php',
+    //         'id' => $id,
+    //         'get' => $request->query->get('test', 'défaut'), //si n y a rien il nous renvoie 'defaut'
+    //         // 'get2' => $_GET['test'] ?? 'defaut',    // ==> ?? on remplace les else
+    //         'session' => get_class($session), 
+    //         'session' => $session->has('cle_inexistante'),
+    //     ]);
+    // }
 }
